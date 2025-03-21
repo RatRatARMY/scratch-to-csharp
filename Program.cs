518c9d3b454ed641aa1c1add3ba34d060bf974bf
RatRatARMY <khang0762412064@outlook.com>
2025-03-21 15:14:23 +0700

    Another code C#

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Nhap chieu dai cua san: ");
+decimal a = decimal.Parse(Console.ReadLine());
+Console.Write("Nhap chieu rong cua san: ");
+decimal b = decimal.Parse(Console.ReadLine());
+decimal S = a * b;
+Console.WriteLine($"Dien tich cua san la: {S}");
