13b43fdbb99d257a9406d396ddd064f8bb2ad017
RatRatARMY <khang0762412064@outlook.com>
2025-03-21 14:46:06 +0700

    Code C# 1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Nhap quang duong (km): ");
+decimal distance = Convert.ToDecimal(Console.ReadLine());
+decimal fare = CalculateFare(distance);
+Console.WriteLine(fare);
+decimal CalculateFare(decimal distance)
+{
+    const decimal rate1 = 14000m;
+    const decimal rate2 = 11000m;
+    const decimal firstLimit = 30m;
+    if (distance <= firstLimit)
+    {
+        return distance * rate1;
+    }
+    else
+    {
+        decimal fare = firstLimit * rate1;
+        fare += (distance - firstLimit) * rate2;
+        return fare;
+    }
+}
